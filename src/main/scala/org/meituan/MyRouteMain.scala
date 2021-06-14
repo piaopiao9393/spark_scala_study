f646636507245c18aafc87151fcf27b6c9ce15c1
zhoushengpu <zhoushengpu@126.com>
2021-06-14 10:18:45 +0800

    first commit

--- src/main/scala/org/meituan/MyRouteMain.scala
+++ src/main/scala/org/meituan/MyRouteMain.scala
@@ -2,11 +2,11 @@ package org.meituan
 
 import org.apache.camel.main.Main
 import org.apache.camel.scala.dsl.builder.RouteBuilderSupport
-
+import org.apache.spark.{SparkConf, SparkContext}
 /**
  * A Main to run Camel with MyRouteBuilder
  */
-object MyRouteMain extends RouteBuilderSupport {
+object MyRouteMain  {
 
   def main(args: Array[String]) {
 //    val main = new Main()
@@ -16,7 +16,14 @@ object MyRouteMain extends RouteBuilderSupport {
 //    main.addRouteBuilder(new MyRouteBuilder(context))
 //    // must use run to start the main application
 //    main.run()
-      println("Hello")
+
+    val conf = new SparkConf().setMaster("local").setAppName("HelloWorld")
+
+    val sc = new SparkContext(conf)
+
+    val helloWorld = sc.parallelize(List("Hello,World!","Hello,Spark!","Hello,BigData!"))
+
+    helloWorld.foreach(line => println(line))
   }
 }
 
